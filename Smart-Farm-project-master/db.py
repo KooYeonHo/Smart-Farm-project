f7d877897972f283592a999b44ecfedb261aeab5
woodfire99 <45327794+woodfire99@users.noreply.github.com>
2022-06-08 14:43:22 +0900

    html 시간추가, 자동 새로고침, db변경

--- Smart-Farm-project-master/db.py
+++ Smart-Farm-project-master/db.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os.path 
+import datetime
 
 db = "data.db"
 
@@ -8,13 +9,15 @@ def setDatabase():
     if not os.path.exists(file):
         con = sqlite3.connect("./db/data.db")
         cur = con.cursor()
-        cur.execute("CREATE TABLE RaspData(water REAL,groundwater REAL,humidity REAL,temperture REAL);")
+        cur.execute("CREATE TABLE RaspData(water REAL,groundwater REAL,humidity REAL,temperture REAL, time TEXT);")
 
 def insertData(water, groundwater, humidity, temperture):
     con = sqlite3.connect("./db/data.db")
     cur = con.cursor()
-    cur.execute("INSERT INTO RaspData (water, groundwater, humidity, temperture) VALUES (?,?,?,?);", 
-    (water, groundwater,humidity, temperture))
+    time = datetime.datetime.now()
+    time = str(time).split(".")[0]
+    cur.execute("INSERT INTO RaspData (water, groundwater, humidity, temperture, time) VALUES (?,?,?,?,?);", 
+    (water, groundwater,humidity, temperture, time))
     con.commit()
 
 def getLastData():
